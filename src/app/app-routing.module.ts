fedd1a44a19ba78d78cdd3e2d1c2eff213c842bd
Scott Bulloch <scottbulloch@Scotts-MacBook-Pro.local>
2019-02-28 16:18:36 -0700

    feat(Routing): add routes and move outlet to proper template location

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { TempComponent } from './temp.component';
+
+const routes: Routes = [
+  {
+    path: 'wizard',
+    loadChildren: './wizard/wizard.module#WizardModule'
+  },
+  {
+    path: '',
+    component: TempComponent
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,17 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'Business Registration & Specialty Licenses'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Business Registration & Specialty Licenses');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Business Registration & Specialty Licenses');
-  });
 });
